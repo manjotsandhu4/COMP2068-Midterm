c53064a04ad375bba0588d7551cb3d0ec0b8b38b
manjotsandhu4 <manjot.sandhu@ymail.com>
2020-07-03 17:07:12 -0400

    Created Actions framework

--- controllers/ReservationsController.js
+++ controllers/ReservationsController.js
@@ -1,1 +1,14 @@
 // You need to complete this controller with the required 7 actions
+//Declaring Viewpath
+const viewPath = 'reservations';
+
+const Reservation = require('../models/reservation');
+const User = require('../models/user');
+
+exports.index = async (req, res) => {};
+exports.show = async (req, res) => {};
+exports.new = async (req, res) => {};
+exports.create = async (req, res) => {};
+exports.edit = async (req, res) => {};
+exports.update = async (req, res) => {};
+exports.delete = async (req, res) => {};
